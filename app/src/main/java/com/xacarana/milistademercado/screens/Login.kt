f10673b43936d048edd8ee79b96ad5f114f106d4
JuanfeP2004 <juanf.ramirezc@upb.edu.co>
2025-04-21 16:05:42 -0500

    Se crearon las estructuras del login y del registro

--- app/src/main/java/com/xacarana/milistademercado/screens/Login.kt
+++ app/src/main/java/com/xacarana/milistademercado/screens/Login.kt
@@ -1,17 +1,42 @@
 package com.xacarana.milistademercado.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import com.xacarana.milistademercado.R
 
 @Composable
 fun Login(navController: NavController){
     Surface() {
-        Text("Login")
-        Button(onClick = {navController.navigate("register")}) {
-            Text("Ir al registro")
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.auth_image),
+                contentDescription = "Imagen del Registro",
+                contentScale = ContentScale.Fit
+            )
+            Text("MI LISTA DE MERCADO")
+
+            Text("INICIAR SESION")
+
+            Text("INGRESA TU NOMBRE")
+            TextField(value = "Usuario", onValueChange = {})
+
+            Text("INGRESA TU CONTRASEÑA")
+            TextField(value = "Contraseña", onValueChange = {})
+
+            Button(onClick = {navController.navigate("register")}) {
+                Text("Ingresar")
+            }
+            Button(onClick = {navController.navigate("register")}) {
+                Text("Crear usuario")
+            }
         }
     }
 }
--- app/src/main/java/com/xacarana/milistademercado/screens/Register.kt
+++ app/src/main/java/com/xacarana/milistademercado/screens/Register.kt
@@ -1,17 +1,43 @@
 package com.xacarana.milistademercado.screens
 
+import android.graphics.drawable.PaintDrawable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import com.xacarana.milistademercado.R
 
 @Composable
 fun Register(navController: NavController){
     Surface() {
-        Text("Register")
-        Button(onClick = {navController.navigate("login")}) {
-            Text("Ir al login")
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.auth_image),
+                contentDescription = "Imagen del Registro",
+                contentScale = ContentScale.Fit
+            )
+            Text("MI LISTA DE MERCADO")
+
+            Text("BIENVENIDO")
+
+            Text("INGRESA TU NOMBRE")
+            TextField(value = "Usuario", onValueChange = {})
+
+            Text("INGRESA TU CONTRASEÑA")
+            TextField(value = "Contraseña", onValueChange = {})
+
+            Button(onClick = {navController.navigate("login")}) {
+                Text("Registrarse")
+            }
+            Button(onClick = {navController.navigate("login")}) {
+                Text("Ya tengo un usuario")
+            }
         }
     }
 }
