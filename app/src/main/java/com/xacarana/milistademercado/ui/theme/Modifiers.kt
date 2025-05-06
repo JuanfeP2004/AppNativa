6189e8d01141f898b515aaed2e6892ada7abc85d
Nicolle Aguirre <nicolle.aguirre@upb.edu.co>
2025-05-05 23:29:05 -0500

    Nuevo archivo de themes para el manejo de espaciado

---
+++ app/src/main/java/com/xacarana/milistademercado/ui/theme/Modifiers.kt
@@ -0,0 +1,7 @@
+package com.xacarana.milistademercado.ui.theme
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+
+val ScreenPadding = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
